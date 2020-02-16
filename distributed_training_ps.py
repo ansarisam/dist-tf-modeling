a28ede200d791c2475ed797cb9607ba11fcc2fd8
ansarisam <ansarisam@gmail.com>
2020-02-16 15:50:38 -0500

    Code cleanup

--- distributed_training_ps.py
+++ distributed_training_ps.py
@@ -35,7 +35,7 @@ BUFFER_SIZE = len(x_train)
 BATCH_SIZE_PER_REPLICA = 16
 GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * 2
 EPOCHS = 10
-STEPS_PER_EPOCH = int(BUFFER_SIZE/EPOCHS)
+STEPS_PER_EPOCH = int(BUFFER_SIZE/EPOCHS/3)
 print("Buffer size: " + str(BUFFER_SIZE) + ", Steps per epoch: " + str(STEPS_PER_EPOCH))
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE,drop_remainder=True)
 test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(GLOBAL_BATCH_SIZE)
